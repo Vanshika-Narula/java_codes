9360b63db303918885ac317f2be52d7f7d3732ff
Vanshika-Narula <narulavanshika704@gmail.com>
2025-08-07 18:49:24 +0530

    roman to int

---
+++ Q41_roman_Integer.java
@@ -0,0 +1,35 @@
+import java.util.HashMap;
+
+public class Q41_roman_Integer {
+
+    public static int romanToInt(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+
+        int res = map.get(s.charAt(s.length()-1));
+        for(int i=s.length()-1; i>0; i--) {
+            if(map.get(s.charAt(i))<=map.get(s.charAt(i-1))) {
+                res+=map.get(s.charAt(i-1));
+            }
+
+            else{
+                res-=map.get(s.charAt(i-1));
+            }
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        String s = "LVIII";
+        int ans = romanToInt(s);
+        System.out.println(ans);
+
+    }
+}
