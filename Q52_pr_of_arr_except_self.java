c2ae84d1c6a49347a9f4d7c486637466905dd41d
Vanshika-Narula <narulavanshika704@gmail.com>
2025-08-12 11:39:47 +0530

    product of array expect self(optimized)

--- Q52_pr_of_arr_except_self.java
+++ Q52_pr_of_arr_except_self.java
@@ -1,32 +1,49 @@
 public class Q52_pr_of_arr_except_self {
 
+    // public static int[] product_array(int[] arr) {
+    //     int n = arr.length;
+    //     int[] left = new int[n];
+    //     int[] right = new int[n];
+    //     left[0] = 1;
+    //     for (int i = 1; i < n; i++) {
+    //         left[i] = left[i - 1] * arr[i - 1];
+    //     }
+
+    //     right[n - 1] = 1;
+    //     for (int i = n - 2; i >= 0; i--) {
+    //         right[i] = right[i + 1] * arr[i + 1];
+    //     }
+
+    //     int[] ans = new int[n];
+    //     for (int i = 0; i < n; i++) {
+    //         ans[i] = left[i] * right[i];
+    //     }
+    //     return ans;
+    // }
+
     public static int[] product_array(int[] arr) {
         int n = arr.length;
-        int[] left = new int[n];
-        int[] right = new int[n];
-        left[0] = 1;
-        for (int i = 1; i < n; i++) {
-            left[i] = left[i - 1] * arr[i - 1];
-        }
-
-        right[n - 1] = 1;
-        for (int i = n - 2; i >= 0; i--) {
-            right[i] = right[i + 1] * arr[i + 1];
-        }
-
         int[] ans = new int[n];
-        for (int i = 0; i < n; i++) {
-            ans[i] = left[i] * right[i];
+
+        ans[0] = 1;
+        for (int i = 1; i < n; i++) {
+            ans[i] = ans[i - 1] * arr[i - 1];
+        }
+
+        int right = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            ans[i] *= right;
+            right *= arr[i];
         }
         return ans;
     }
+    
     public static void main(String[] args) {
         int[] arr = { 1, 2, 3, 4 };
         int[] ans = product_array(arr);
         for (int i : ans) {
-            System.out.print(i+" ");
+            System.out.print(i + " ");
         }
-        
 
     }
 }
