25c7aed79c63ad502dc409b96c18224cbe26c86a
Vanshika-Narula <narulavanshika704@gmail.com>
2025-09-02 19:32:56 +0530

    length

--- linked_list/convertArr.java
+++ linked_list/convertArr.java
@@ -21,7 +21,7 @@ public class convertArr {
     public static Node convertArr2LL(int[] arr) {
         Node head = new Node(arr[0]);
         Node mover = head;
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = 1; i < arr.length; i++) {
             Node temp = new Node(arr[i]);
             mover.next = temp;
             mover = temp;
@@ -37,6 +37,18 @@ public class convertArr {
         }
     }
 
+    public static int lengthLL(Node head) {
+        if(head==null)
+            return 0;
+        int cnt = 0;
+        Node temp = head;
+        while (temp != null) {
+            cnt++;
+            temp = temp.next;
+        }
+        return cnt;
+    }
+
     public static Node insertAtHead(Node head, int val) {
         Node newNode = new Node(val);
         if (head == null)
@@ -50,6 +62,10 @@ public class convertArr {
         int[] arr = { 2, 4, 6, 8, 10, 12 };
         Node head = convertArr2LL(arr);
         printLL(head);
+        System.out.println();
+        int length = lengthLL(head);
+        System.out.println(length);
+        System.out.println();
     }
     
 }
